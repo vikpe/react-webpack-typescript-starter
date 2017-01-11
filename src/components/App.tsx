ded4f3642412ec3aa9c07645f0b0b0e6ca1d4eb6
Viktor Persson <viktor.persson@arcsin.se>
2017-01-11 16:27:56 +0100

    Added example of including CSS.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 
+require('!style!css!./App.css');
+
 export interface AppProps {
 }
 
 export default class App extends React.Component<AppProps, undefined> {
     render() {
-        return <div>
+        return <div className="app">
             <h1>Hello World!</h1>
-            <p>Foo to the bar</p>
+            <p>Foo to the barz</p>
         </div>;
     }
 }
