ed04add3bd6ff3e9d43c8ec66061e6282bfd8c83
Viktor Persson <viktor.persson@arcsin.se>
2020-07-19 11:34:06 +0200

    Update webpack dev config, remove obsolete/deprecated plugin.

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -17,6 +17,5 @@ module.exports = merge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-    new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
   ],
 });
