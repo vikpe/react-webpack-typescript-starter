6b87612f8de6b616ef1acb6fb3a0327befd89131
Viktor Persson <viktor.persson@arcsin.se>
2021-02-23 16:59:11 +0100

    Update eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: "tsconfig.json",
         sourceType: "module",
         ecmaVersion: 2020,
         ecmaFeatures: {
