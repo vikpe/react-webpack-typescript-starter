77f2f532b5ac1299b3749e036d9d1d92c075852f
Viktor Persson <viktor.persson@arcsin.se>
2021-02-23 17:17:17 +0100

    Remove obsolete entry from eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
     extends: [
         "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
         "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors.
-        "prettier"
+        "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors.
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
