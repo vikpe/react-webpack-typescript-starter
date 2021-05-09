8cbcb8c2d0109843f62e2c008150f96cae137b99
naftalimurgor <murgornaftali@gmail.com>
2021-05-09 16:12:11 +0300

    Fix Error 404 related errors when using React Router
    When using React Router: Refresh and links don't work smoothly: gives error cannot `GET /some-path`

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -13,6 +13,7 @@ module.exports = merge(commonConfig, {
   ],
   devServer: {
     hot: true, // enable HMR on the server
+    historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url 
   },
   devtool: "cheap-module-source-map",
   plugins: [
