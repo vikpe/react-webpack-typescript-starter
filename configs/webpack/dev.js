66e0890a842df3d2fd8b7ec11ae5614767ffb1c9
Viktor Persson <viktor.persson@arcsin.se>
2021-08-26 08:53:11 +0200

    Update webpack-dev-server.

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -1,6 +1,5 @@
 // development config
 const { merge } = require("webpack-merge");
-const webpack = require("webpack");
 const commonConfig = require("./common");
 
 module.exports = merge(commonConfig, {
@@ -8,15 +7,12 @@ module.exports = merge(commonConfig, {
   entry: [
     "react-hot-loader/patch", // activate HMR for React
     "webpack-dev-server/client?http://localhost:8080", // bundle the client for webpack-dev-server and connect to the provided endpoint
-    "webpack/hot/only-dev-server", // bundle the client for hot reloading, only- means to only hot reload for successful updates
     "./index.tsx", // the entry point of our app
   ],
   devServer: {
-    hot: true, // enable HMR on the server
+    hot: "only", // enable HMR on the server
     historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url 
   },
   devtool: "cheap-module-source-map",
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-  ],
+  plugins: [],
 });
