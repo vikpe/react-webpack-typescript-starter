44407d62c4d8df92697bb1f494628d3239794cef
Viktor Persson <viktor.persson@arcsin.se>
2022-04-16 17:26:58 +0200

    Update babel config.

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,11 @@
 module.exports = (api) => {
   api.cache.using(() => process.env.NODE_ENV);
-  const isDevelopment = api.env("development");
-  const plugins = [];
-
-  if (isDevelopment) {
-    plugins.concat("react-refresh/babel");
-  }
 
   return {
     presets: [
       "@babel/env",
-      ["@babel/preset-react", { runtime: "automatic" }],
+      ['@babel/preset-react', {development: !api.env('production'), runtime: 'automatic'}],
       "@babel/preset-typescript",
-    ],
-    plugins,
+    ]
   };
 };
